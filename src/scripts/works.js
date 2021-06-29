810b116ca281acae090246227a42d910788410c8
Rozhkov Georgiy <rozkovg70@gmail.com>
2021-06-29 22:29:16 +0300

    разбил на компоненты

--- src/scripts/works.js
+++ src/scripts/works.js
@@ -1,45 +1,94 @@
 import Vue from "vue";
 
-
 const thumbs = {
-    template: "#preview-thumbs",
-}
+  props: ["works", "currentWork"],
+  template: "#preview-thumbs",
+};
+
 const btns = {
-    template: "#preview-btns",
-}
-const tags = {
-    template: "#preview-tags",
-}
+  template: "#preview-btns",
+};
+
 const display = {
-    template: "#preview-display",
-    components: { thumbs, btns },
-}
+  props: ["currentWork", "works", "currentIndex"],
+  template: "#preview-display",
+  components: { thumbs, btns },
+  computed: {
+    reversedWorks() {
+      const works = [...this.works];
+      return works.slice(0, 3).reverse();
+    },
+  },
+};
+
+const tags = {
+  props: ["tags"],
+  template: "#preview-tags",
+};
+
 const info = {
-    template: "#preview-info",
-    components: { tags },
-}
+  props: ["currentWork"],
+  template: "#preview-info",
+  components: { tags },
+  computed: {
+    tagsArray() {
+      return this.currentWork.skills.split(",");
+    }
+  }
+};
+
 new Vue({
-    el: "#preview-component",
-    template: "#preview-container",
-    components: { display, info },
-    data(){
-        return {
-            works: []
-        }
+  el: "#preview-component",
+  template: "#preview-container",
+  components: { display, info },
+  data() {
+    return {
+      works: [],
+      currentIndex: 0,
+    };
+  },
+  computed: {
+    currentWork() {
+      return this.works[0];
     },
-    methods: {
-        requireImagesToArray(data){
-            return data.map(item =>{
-                const requiredImage = require(`../images/content/{item.photo}`).default;
-                item.photo = requiredImage;
-                return item 
-            })
-        }
+  },
+  watch: {
+    currentIndex(value) {
+      this.makeInfiniteLoopForNdx(value);
     },
-    created(){
-        
-        const data = require("../data/works.json");
-        debugger
-        this.works = requireImagesToArray(data);
+  },
+  methods: {
+    makeInfiniteLoopForNdx(index) {
+      const worksNumber = this.works.length - 1;
+      if (index < 0) this.currentIndex = worksNumber;
+      if (index > worksNumber) this.currentIndex = 0;
     },
-})
+    requireImagesToArray(data) {
+      return data.map((item) => {
+        const requiredImage = require(`../images/content/${item.photo}`)
+          .default;
+        item.photo = requiredImage;
+        return item;
+      });
+    },
+    slide(direction) {
+      const lastItem = this.works[this.works.length - 1];
+      switch (direction) {
+        case "next":
+          this.works.push(this.works[0]);
+          this.works.shift();
+          this.currentIndex++;
+          break;
+        case "prev":
+          this.works.unshift(lastItem);
+          this.works.pop();
+          this.currentIndex--;
+          break;
+      }
+    },
+  },
+  created() {
+    const data = require("../data/works.json");
+    this.works = this.requireImagesToArray(data);
+  },
+});
